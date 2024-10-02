753722345073971966b819dcce7d61e214f6a895
Douglas Cabreira <dougc.gonca@gmail.com>
2024-10-01 22:02:32 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
-
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
